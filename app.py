af55e8a5894277a0aeb64aae9923882f13b44799
Collin Swisher <collin.swisher@hotmail.com>
2026-01-28 13:37:46 -0600

    Add password authentication

--- app.py
+++ app.py
@@ -1,25 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
+import hashlib
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
 message = "raised"
+password = "easy password"
+phash = hashlib.sha256(bytes(password, encoding="utf-8")).hexdigest()
+
+def verifyPassword(passw):
+    return hashlib.sha256(bytes(passw, encoding='utf-8')).hexdigest() == phash
 
 @app.route("/")
 def index():
-    return render_template("index.html", message=message)
+    return render_template("index.html", message=message, phash=phash)
 
-@socketio.on("raise")
+@app.route("/raise", methods=["POST"])
 def guardRaise():
     global message
-    message = "raised"
-    emit("raised", message, broadcast=True, namespace="/")
+    data = request.json
+    if "password" in data and verifyPassword(data["password"]):
+        message = "raised"
+        emit("raised", message, broadcast=True, namespace="/")
+        return "raised"
+    return "incorrect password"
 
-@socketio.on("lower")
+@app.route("/lower", methods=["POST"])
 def guardLower():
     global message
-    message = "lowered"
-    emit("lowered", message, broadcast=True, namespace="/")
+    data = request.json
+    if "password" in data and verifyPassword(data["password"]):
+        message = "lowered"
+        emit("lowered", message, broadcast=True, namespace="/")
+        return "lowered"
+    else:
+        return "incorrect password"
 
 socketio.run(app, debug=True)
--- static/script.js
+++ static/script.js
@@ -1,5 +1,9 @@
 const status_element = document.querySelector("#status")
+const password_input = document.querySelector("#pass")
+const hash = document.querySelector("meta[name='hash']").content
 const socket = io()
+
+console.log(hash)
 
 socket.on("raised", (msg) => {
     status_element.innerHTML = msg
@@ -10,9 +14,41 @@ socket.on("lowered", (msg) => {
 })
 
 async function raise() {
-    socket.emit("raise")
+    sendRequest("/raise")
 }
 
 async function lower() {
-    socket.emit("lower")
+    sendRequest("/lower")
+}
+
+/**
+ * 
+ * @param {ArrayBuffer} data 
+ * @returns {string}
+ */
+function toHex(data) {
+    return Array.from(new Uint8Array(data)).map(b => b.toString(16).padStart(2, '0')).join('')
+}
+
+/**
+ * 
+ * @param {string} msg 
+ */
+function displayMessage(msg) {
+    status_element.innerHTML = msg
+}
+
+/**
+ * 
+ * @param {string} path 
+ */
+async function sendRequest(path) {
+    const new_hash = toHex(await crypto.subtle.digest("SHA-256", new TextEncoder().encode(password_input.value)))
+    if (hash == new_hash) {
+        const res = await fetch(path, {method: "POST", body: JSON.stringify({password: password_input.value}), headers: {"Content-Type": "application/json"}})
+        displayMessage(await res.text())
+    }
+    else {
+        displayMessage("Incorrect password")
+    }
 }
